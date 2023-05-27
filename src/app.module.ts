d78a4a410cc34f1ef74d8d1faebdbd8253e70531
KazantsevaJane <103194481+KazantsevaJane@users.noreply.github.com>
2023-05-27 16:07:48 +0500

    chto-to

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,14 @@ import {UsersModule} from "./users/users.module";
 import {MongooseModule} from "@nestjs/mongoose";
 import {UsersService} from "./users/users.service";
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import * as process from "process";
 
 @Module({
   imports: [
+      ConfigModule.forRoot({envFilePath: ['.env']}),
       UsersModule,
-      MongooseModule.forRoot(`mongodb+srv://cerebro0305:WundEr741@cluster0.y44pgmf.mongodb.net/?retryWrites=true&w=majority`),
+      MongooseModule.forRoot(process.env.MONGO_URI),
       AuthModule
   ],
   controllers: [AppController],
