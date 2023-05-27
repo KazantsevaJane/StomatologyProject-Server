28ccbdaf56b7fcd4b59b1b3dc7652648e34e5fb6
KazantsevaJane <103194481+KazantsevaJane@users.noreply.github.com>
2023-05-27 10:32:52 +0500

    add public

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000
+  await app.listen(PORT);
 }
 bootstrap();
