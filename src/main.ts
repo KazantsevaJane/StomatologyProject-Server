bffb4583769402b2f63ecb9532fdbe07132a6f39
KazantsevaJane <103194481+KazantsevaJane@users.noreply.github.com>
2023-05-27 16:18:40 +0500

    Change port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as process from "process";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const PORT = process.env.PORT || 3000
+  const PORT = 3000
   await app.listen(PORT);
 }
 bootstrap();
